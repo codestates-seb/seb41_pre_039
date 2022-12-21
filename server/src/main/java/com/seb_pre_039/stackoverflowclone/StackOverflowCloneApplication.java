88fb74815f79104fe9195116886524632861e435
injongjeon <111412215+jnjongjeon@users.noreply.github.com>
2022-12-21 15:47:31 +0900

    docs: created project

---
+++ server/src/main/java/com/seb_pre_039/stackoverflowclone/StackOverflowCloneApplication.java
@@ -0,0 +1,13 @@
+package com.seb_pre_039.stackoverflowclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackOverflowCloneApplication.class, args);
+    }
+
+}

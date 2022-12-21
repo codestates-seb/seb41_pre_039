5df4a2fa85195256256e1bd443e5344fea06fac2
injongjeon <jeonij23@gmail.com>
2022-12-21 15:25:23 +0900

    [CREATE] project

---
+++ server/src/main/java/com/seb41_pre_039/stackoverflowclone/StackoverflowcloneApplication.java
@@ -0,0 +1,13 @@
+package com.seb41_pre_039.stackoverflowclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowcloneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb41_pre_039/stackoverflowclone/StackoverflowcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb41_pre_039.stackoverflowclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

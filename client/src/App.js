45da1334cf532a334cae8f88cdb30d01bf105379
Seung gu Shin <less0805@gmail.com>
2022-12-22 13:59:04 +0900

    feat: Add layout - header, sidebar, content, footer (#24)
    Co-authored-by: Seung gu Shin <77656241+ninefloor@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import Header from './components/Header.js';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="header">Header</div>
+
+      <div className="container">
+        <div className="sidebar">Sidebar</div>
+        <div className="content">Main</div>
+      </div>
+      <div className="footer">Footer</div>
     </div>
   );
 }
-
 export default App;

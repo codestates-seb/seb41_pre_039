36fd83f742f07997800add2feb216a6945b1d255
gkswldus <gkswldus1108@naver.com>
2022-12-22 11:23:50 +0900

    feat: Add layout - header, sidebar, content, footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
 function App() {
-  return <div className="App">hello world!</div>;
-}
+  return (
+    <div className="App">
+      <div className="header">Header</div>
 
+      <div className="container">
+        <div className="sidebar">Sidebar</div>
+        <div className="content">Main</div>
+      </div>
+
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
 export default App;

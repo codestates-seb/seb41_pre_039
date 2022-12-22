a21c54ae8d19628595ad9911d98d7a77a565a82d
Seung gu Shin <77656241+ninefloor@users.noreply.github.com>
2022-12-22 16:37:08 +0900

    fix: add components for layout and fix styles (#25)
    * feat: Add layout - header, sidebar, content, footer (#24)
    
    Co-authored-by: Seung gu Shin <77656241+ninefloor@users.noreply.github.com>
    
    * feature: add react-router-dom package
    
    * fix: add components for layout and fix styles
    
    * fix: add media-query styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import Header from './components/Header.js';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">Header</div>
+      <div className="header">
+        <Header />
+      </div>
 
       <div className="container">
-        <div className="sidebar">Sidebar</div>
-        <div className="content">Main</div>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="content">
+          {/* 내용을 채우기 위한 임시 요소 */}
+          <div>pages</div>
+        </div>
       </div>
-      <div className="footer">Footer</div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }

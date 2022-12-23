d9b2a2e7efee519b5b537efcee48609cb4577a00
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-12-23 20:58:19 +0900

    feat: create YourAnswer modal

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -1,4 +1,35 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './Answer.css';
+
+const NextButton = styled.button`
+  background-color: hsl(206deg 100% 52%);
+  color: #fff;
+  cursor: pointer;
+  border: 1px soContentLid transparent;
+  box-shadow: inset 0 1px 0 0 hsl(206deg 100% 52%),
+    inset 0 2px 0 0 hsla(0, 0%, 100%, 0.4);
+  border-radius: 3px;
+  font-size: 13px;
+  padding: 0.8em;
+  margin-top: 16px;
+  &:hover {
+    background-color: hsl(206deg 100% 40%);
+  }
+`;
+
+const ContentLi = styled.li`
+  list-style-type: disc;
+`;
+
+const ContentP = styled.p`
+  margin-bottom: 13px;
+`;
+
+const ContentUl = styled.ul`
+  margin-bottom: 13px;
+  margin-left: 30px;
+`;
 
 export default function Answer() {
   return (
@@ -26,9 +57,9 @@ export default function Answer() {
           </div>
           <article className="content-question">
             <p className="answer-p">
-              Is there a way to change the color of the following slide of the
-              CRAN package shinyWidgets? Thanks in advance. I need to do it also
-              in within the update function.
+              Is there a way to change the color of the following sContentLide
+              of the CRAN package shinyWidgets? Thanks in advance. I need to do
+              it also in within the update function.
             </p>
             <div className="answer-codeBox">
               <div className="answer-code">
@@ -70,12 +101,67 @@ export default function Answer() {
 }
 
 function YourAnswer() {
+  const [help, setHelp] = useState(false);
   return (
-    <form className="ya-box">
-      <div className="ya-title">
-        <h2>Your Answer</h2>
-      </div>
-      <textarea className="ya-textarea"></textarea>
-    </form>
+    <>
+      <form className="ya-box">
+        <div className="ya-title">
+          <h2>Your Answer</h2>
+        </div>
+        <textarea
+          onClick={() => setHelp(true)}
+          className="ya-textarea"
+        ></textarea>
+        {help ? <YourAnswerModal /> : null}
+        <NextButton>Post Your Answer</NextButton>
+      </form>
+    </>
+  );
+}
+
+function YourAnswerModal() {
+  const [none, setNone] = useState(false);
+  return (
+    <>
+      <aside
+        className={none ? 'ya-modal-container hide' : 'ya-modal-container'}
+      >
+        <div className="ya-modal-content">
+          <ContentP>
+            Thanks for contributing an answer to Stack Overflow!
+          </ContentP>
+          <ContentUl>
+            <ContentLi>
+              Please be sure to <em>answer the question.</em> Provide details
+              and share your research!
+            </ContentLi>
+          </ContentUl>
+          <ContentP>
+            But <em>avoid</em> …
+          </ContentP>
+          <ContentUl>
+            <ContentLi>
+              Asking for help, clarification, or responding to other answers.
+            </ContentLi>
+            <ContentLi>
+              Making statements based on opinion; back them up with references
+              or personal experience.
+            </ContentLi>
+          </ContentUl>
+          <p>To learn more, see our tips on writing great answers.</p>
+        </div>
+        <div className="ya-modal-exit">
+          <span
+            onClick={() => {
+              setNone(!none);
+            }}
+            role="presentation"
+            className="ya-exit"
+          >
+            ✕
+          </span>
+        </div>
+      </aside>
+    </>
   );
 }

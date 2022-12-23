6e645921ad022ff0988fdf5ccfb845cbdfdcb59e
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-12-23 10:46:45 +0900

    style: semi refactoring

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,28 +1,21 @@
 import Answer from '../components/Answer';
 import Question from '../components/Question';
-import './Post.css';
+import styled from 'styled-components';
+
+/* content 컨테이너 */
+const ContentContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function Post() {
   return (
-    <div className="content-container">
-      <div className="content-header">
-        <div className="content-title">
-          <span className="title-h">
-            <h1>Change color of Slider Text Input Widget in Shiny R?</h1>
-          </span>
-          <div className="question-ask">
-            <button className="ask-button">Ask Question</button>
-          </div>
-        </div>
-        <div className="question-info">
-          <span>Asked</span>
-          <span>Modified</span>
-          <span>Viewed</span>
-        </div>
-        <Question />
-        <h2>1 Answer</h2>
-        <Answer />
-      </div>
-    </div>
+    <ContentContainer>
+      <Question />
+      <Answer />
+    </ContentContainer>
   );
 }

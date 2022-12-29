9cb5f2185307e6581e42cdcddf24ac835d526846
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-12-29 19:17:22 +0900

    feat: create Edit component

---
+++ client/src/Edit.js
@@ -0,0 +1,10 @@
+import EditAnswer from './components/EditAnswer';
+import EditQuestion from './components/EditQuestion';
+
+export default function Edit() {
+  return (
+    <>
+      <EditAnswer />
+    </>
+  );
+}
---
+++ client/src/components/EditAnswer.js
@@ -0,0 +1,145 @@
+import styled from 'styled-components';
+import { useState, useRef, useEffect } from 'react';
+import { ContentEditor } from '../components/Editor';
+import './Edit.css';
+import { answer } from './initialState';
+
+const InputBox = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  background-color: #fff;
+  color: #0c0d0e;
+  text-align: left;
+  margin-bottom: 24px;
+  position: relative;
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+    cursor: pointer;
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 9px;
+    outline: none;
+    border: 1px solid rgb(193, 193, 193);
+    border-radius: 3px;
+    &:focus {
+      border-color: rgb(0 116 204);
+      box-shadow: 0 0 0 4px rgb(0 116 204 / 15%);
+    }
+    &::placeholder {
+      color: #ccc;
+    }
+  }
+  .validation {
+    font-size: 12px;
+    color: #ab262a;
+  }
+
+  .answer-title {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+`;
+
+const Button = styled.button`
+  color: #fff;
+  border: 1px solid transparent;
+  background-color: hsl(206deg 100% 52%);
+  box-shadow: inset 0 0.4px 0 0 hsl(206deg 100% 52%),
+    inset 0 1px 0 0 hsla(0, 0%, 100%, 0.4);
+  border-radius: 3px;
+  font-size: 13px;
+  padding: 0.8em;
+  margin-top: 16px;
+  &:hover {
+    cursor: pointer;
+    background-color: hsl(206deg 100% 40%);
+  }
+`;
+
+const CancelButton = styled(Button)`
+  background-color: transparent;
+  box-shadow: none;
+  color: #0074cc;
+  margin-left: 20px;
+  &:hover {
+    background-color: #eff8ff;
+  }
+`;
+
+export default function EditQuestion({ setIsKey }) {
+  const [body, setbody] = useState(answer[0].content);
+  const [summary, setSummary] = useState('');
+
+  const bodyRef = useRef(null);
+
+  useEffect(() => {
+    if (localStorage.draft) {
+      console.log(localStorage.draft);
+      const draft = JSON.parse(localStorage.getItem('draft'));
+      setbody(draft.body || '');
+    }
+  }, []);
+
+  const deleteDraftHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('draft');
+    location.reload();
+  };
+
+  return (
+    <section className="edit--container">
+      <article className="edit--advice">
+        <p>Your edit will be placed in a queue until it is peer reviewed.</p>
+        <p>
+          We welcome edits that make the post easier to understand and more
+          valuable for readers. Because community members review edits, please
+          try to make the post substantially better than how you found it, for
+          example, by fixing grammar or adding additional resources and
+          hyperlinks.
+        </p>
+      </article>
+      <form id="edit--form">
+        <InputBox>
+          <label htmlFor="body" className="answer-title">
+            Answer
+          </label>
+          <ContentEditor
+            id="body"
+            value={body}
+            changeHandler={setbody}
+            refer={bodyRef}
+          />
+        </InputBox>
+        <InputBox>
+          <label htmlFor="summary">Edit Summary</label>
+          <input
+            id="summary"
+            type="text"
+            placeholder="briefly explain your changes (corrected spelling, fixed, grammar, improved formatting)"
+            value={summary}
+            onChange={(e) => setSummary(e.target.value)}
+          />
+        </InputBox>
+        <Button
+          type="form"
+          form="edit--form"
+          className="edit--submit submitButton"
+        >
+          Save edits
+        </Button>
+        <CancelButton
+          className="edit--draft-discard submitButton"
+          onClick={(e) => deleteDraftHandler(e)}
+        >
+          Cancel
+        </CancelButton>
+      </form>
+    </section>
+  );
+}

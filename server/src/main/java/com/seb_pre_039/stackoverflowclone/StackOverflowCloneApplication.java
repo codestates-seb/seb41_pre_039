2a383979e9b95ce05c3e6de7ee792460b4a36b02
userkim12 <potomq444@naver.com>
2022-12-30 17:58:28 +0900

    fix: Update for build

--- server/src/main/java/com/seb_pre_039/stackoverflowclone/StackOverflowCloneApplication.java
+++ server/src/main/java/com/seb_pre_039/stackoverflowclone/StackOverflowCloneApplication.java
@@ -2,14 +2,21 @@ package com.seb_pre_039.stackoverflowclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class StackOverflowCloneApplication {
+public class StackOverflowCloneApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StackOverflowCloneApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StackOverflowCloneApplication.class);
     }
 
 }

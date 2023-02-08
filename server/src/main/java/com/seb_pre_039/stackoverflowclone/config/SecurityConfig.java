367b23b61cf1d08d2b189a5a2c3ac0fddbb3e4b6
userkim12 <potomq444@naver.com>
2023-02-08 22:10:31 +0900

    feat: add request's authorization

--- server/src/main/java/com/seb_pre_039/stackoverflowclone/config/SecurityConfig.java
+++ server/src/main/java/com/seb_pre_039/stackoverflowclone/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import com.seb_pre_039.stackoverflowclone.auth.jwt.JwtTokenizer;
 import com.seb_pre_039.stackoverflowclone.auth.utils.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -56,7 +57,17 @@ public class SecurityConfig {
                 .apply(new CustomFilterConfigurer())
                 .and()
                 .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().permitAll()
+                        authorize
+                                .antMatchers(HttpMethod.POST, "/members").permitAll()
+                                .antMatchers(HttpMethod.PATCH, "/members/*").hasRole("USER")
+                                .antMatchers(HttpMethod.GET, "/members/*").hasRole("USER")
+                                .antMatchers(HttpMethod.GET, "/members").hasRole("USER")
+                                .antMatchers(HttpMethod.DELETE, "/members/*").hasAnyRole("USER", "ADMIN")
+                                .antMatchers(HttpMethod.POST, "/questions").hasRole("USER")
+                                .antMatchers(HttpMethod.PATCH, "/questions/*").hasRole("USER")
+                                .antMatchers(HttpMethod.GET, "/questions/**").hasAnyRole("USER", "ADMIN")
+                                .antMatchers(HttpMethod.DELETE, "/questions/*").hasRole("USER")
+                                .antMatchers(HttpMethod.DELETE, "/questions").hasRole("ADMIN")
                 );
 
 
@@ -74,8 +85,11 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
